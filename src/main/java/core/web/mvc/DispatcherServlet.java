337e88fe15e4c0bacfae874a5c83c9f6549f4c24
Lee-jong-won <78682445+Lee-jong-won@users.noreply.github.com>
2025-08-05 11:06:40 +0900

    최종 커밋

--- src/main/java/core/web/mvc/DispatcherServlet.java
+++ src/main/java/core/web/mvc/DispatcherServlet.java
@@ -1,5 +1,8 @@
 package core.web.mvc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,6 +15,7 @@ import java.io.IOException;
 public class DispatcherServlet extends HttpServlet {
 
     private static final String DEFAULT_REDIRECT_PREFIX = "redirect:";
+    private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
@@ -23,9 +27,16 @@ public class DispatcherServlet extends HttpServlet {
 
         try {
             viewName = controller.exeucte(req, resp);
+            move(viewName, req, resp);
         } catch (Exception e) {
+            log.error("Exception : {}", e);
             throw new RuntimeException(e);
         }
+
+    }
+
+    private void move(String viewName, HttpServletRequest req,
+                      HttpServletResponse resp) throws IOException, ServletException {
 
         if(viewName.startsWith(DEFAULT_REDIRECT_PREFIX))
             resp.sendRedirect(viewName.substring(DEFAULT_REDIRECT_PREFIX.length()));
